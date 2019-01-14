3319ccbd1ec08c1c8cde8cc52c50426b4ee5fb39
Omar Flores <cerberusprogrammer@gmail.com>
2019-01-14 12:51:24 -0800

    Now you can add some task in the table.

--- src/sample/ControllerHome.java
+++ src/sample/ControllerHome.java
@@ -1,14 +1,18 @@
 package sample;
 
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
 
+import java.awt.event.ActionEvent;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class ControllerHome implements Initializable {
@@ -19,6 +23,8 @@ public class ControllerHome implements Initializable {
     private TableColumn<Task, String> tablecolumnTask;
     @FXML
     private TableView<Task> tableviewTask;
+    @FXML
+    private TableColumn<Task, String> tablecolumnAccomplished;
     @FXML
     private Pane paneBar;
     @FXML
@@ -34,11 +40,24 @@ public class ControllerHome implements Initializable {
     @FXML
     private RadioButton radiobuttonTheme;
 
-    private Boolean theme = false;
-
     @FXML
     void addTask(KeyEvent event) {
+        if (event.getCode().equals(KeyCode.ENTER)) {
+            Task task = new Task(new CheckBox(""), textfieldTask.getText(), "");
+            Data.addTask(task);
+            refreshTable();
+        }
+    }
 
+    public void addTaskButton(javafx.event.ActionEvent event) {
+        Task task = new Task(new CheckBox(""), textfieldTask.getText(), "");
+        Data.addTask(task);
+        refreshTable();
+    }
+
+    private void refreshTable() {
+        List<Task> listPersona = Data.getListTask();
+        tableviewTask.setItems(FXCollections.observableArrayList(listPersona));
     }
 
     @FXML
@@ -67,6 +86,11 @@ public class ControllerHome implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        tablecolumnTask.setCellValueFactory(new PropertyValueFactory<>("id"));
+        tablecolumnAccomplished.setCellValueFactory(new PropertyValueFactory<>("accomplished"));
+        tablecolumnTask.setCellValueFactory(new PropertyValueFactory<>("text"));
+
+        List<Task> listPersona = Data.getListTask();
+
+        tableviewTask.setItems(FXCollections.observableArrayList(listPersona));
     }
 }
--- src/sample/Data.java
+++ src/sample/Data.java
@@ -1,4 +1,21 @@
 package sample;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Data {
+
+    public static List<Task> listTask = new ArrayList<>();
+
+    public static List<Task> getListTask() {
+        return listTask;
+    }
+
+    public static void addTask(Task task) {
+        listTask.add(task);
+    }
+
+    public static void setListTask(List<Task> listTask) {
+        Data.listTask = listTask;
+    }
 }
