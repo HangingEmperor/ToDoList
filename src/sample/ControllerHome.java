2f0ec3a7e8eec13aa1f81c4d6f25cf80896e0300
Omar Flores <cerberusprogrammer@gmail.com>
2019-01-14 09:20:47 -0800

    New options added.

--- src/sample/ControllerHome.java
+++ src/sample/ControllerHome.java
@@ -1,42 +1,40 @@
 package sample;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
 
-public class ControllerHome {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ControllerHome implements Initializable {
 
     @FXML
     private Pane paneBackground;
-
     @FXML
-    private TableColumn<?, ?> tablecolumnTask;
-
+    private TableColumn<Task, String> tablecolumnTask;
     @FXML
-    private TableView<?> tableviewTask;
-
+    private TableView<Task> tableviewTask;
     @FXML
     private Pane paneBar;
-
     @FXML
     private ImageView imageviewClose;
-
     @FXML
     private ImageView imageviewExpand;
-
     @FXML
     private ImageView imageviewMinimize;
-
     @FXML
     private Label labelTitle;
-
     @FXML
     private TextField textfieldTask;
-
     @FXML
     private RadioButton radiobuttonTheme;
+
+    private Boolean theme = false;
 
     @FXML
     void addTask(KeyEvent event) {
@@ -45,12 +43,16 @@ public class ControllerHome {
 
     @FXML
     void changeTheme() {
-
+        if (radiobuttonTheme.isSelected()) {
+            paneBackground.setStyle("-fx-background-color: black;");
+        } else {
+            paneBackground.setStyle("-fx-background-color: white;");
+        }
     }
 
     @FXML
     void closeWindow() {
-
+        System.exit(0);
     }
 
     @FXML
@@ -61,5 +63,10 @@ public class ControllerHome {
     @FXML
     void minimizeWindow() {
 
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        tablecolumnTask.setCellValueFactory(new PropertyValueFactory<>("id"));
     }
 }
---
+++ src/sample/Task.java
@@ -0,0 +1,66 @@
+package sample;
+
+import javafx.scene.control.CheckBox;
+
+import java.util.Objects;
+
+public class Task {
+
+    private CheckBox accomplished;
+    private String text;
+    private String date;
+
+    public Task(CheckBox accomplished, String text, String date) {
+        this.accomplished = accomplished;
+        this.text = text;
+        this.date = date;
+    }
+
+    public CheckBox getAccomplished() {
+        return accomplished;
+    }
+
+    public void setAccomplished(CheckBox accomplished) {
+        this.accomplished = accomplished;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Task task = (Task) o;
+        return Objects.equals(accomplished, task.accomplished) &&
+                Objects.equals(text, task.text) &&
+                Objects.equals(date, task.date);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(accomplished, text, date);
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "accomplished=" + accomplished +
+                ", text='" + text + '\'' +
+                ", date='" + date + '\'' +
+                '}';
+    }
+}
